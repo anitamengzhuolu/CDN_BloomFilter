412b58e0c63e54e4ef2502651977336527767753
Adarsh <am4417@columbia.edu>
2016-12-20 01:16:59 -0500

    script to plot the theoretical p vs k graph

---
+++ src/scripts/bloomFilterGraph.py
@@ -0,0 +1,13 @@
+#p = exp(-(m/n)*pow(ln(2),2))
+import math
+import matplotlib.pyplot as plt
+
+m = 80000
+n = 0.1 * m
+p = []
+
+for k in range(1,100):
+    fp = math.pow(1-math.exp(-k*n/m),k)
+    p.append(fp)
+plt.plot(p)
+plt.show()
